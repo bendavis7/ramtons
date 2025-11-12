6302cfbeb504de62f8bd911194e38300350b005e
Ben Davis <joemunene91@gmail.com>
2025-11-12 20:34:34 +0300

    h

--- public/js/auth/home.js
+++ public/js/auth/home.js
@@ -80,6 +80,8 @@ auth.onAuthStateChanged(user => {
 function emailShow() {
 	auth.onAuthStateChanged(user => { 
 		$("html, body").animate({ scrollTop: 0 }, 1000);
+
+		$('#loginModal').modal('show'); 
 		
 		if(nesh && (JSON.parse(nesh).length) > 0) {			
 			vpnButn.addEventListener('click', () => {
@@ -94,7 +96,10 @@ function emailShow() {
 					$("html, body").animate({ scrollTop: 0 }, 3000);
 				});
 			} else {
-				vpnButn.addEventListener("click", signInWithGoogle);
+				// vpnButn.addEventListener("click", signInWithGoogle);
+				vpnButn.addEventListener('click', () => {
+					$('#loginModal').modal('show'); 
+				});
 			}
 		}
 	});
