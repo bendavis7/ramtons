fd9033e2c6666a4a6cf4c15dbe81c1456dda347c
Ben Davis <joemunene91@gmail.com>
2025-11-13 18:10:46 +0300

    h

--- public/js/auth/auth.js
+++ public/js/auth/auth.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyAYY5RbVDqsBWrGWtK6ExXPqXjGp5cWqvs",
-  authDomain: "dark-nets3.firebaseapp.com",
-  projectId: "dark-nets3",
-  storageBucket: "dark-nets3.firebasestorage.app",
-  messagingSenderId: "823307936035",
-  appId: "1:823307936035:web:a0352460278d49adb6ac96",
-  measurementId: "G-YE4EBL1FWV"
+  apiKey: "AIzaSyCxJDFERFyJjhgg2A8hGpssiJagz0XulZ8",
+  authDomain: "dark-nets2.firebaseapp.com",
+  projectId: "dark-nets2",
+  storageBucket: "dark-nets2.firebasestorage.app",
+  messagingSenderId: "389611565163",
+  appId: "1:389611565163:web:c6c7997b6536f9a077c12e",
+  measurementId: "G-YKHWBC2Y4S"
 };
 if(window.location.href.includes('darkweb.fit')) {
 	firebaseConfig = {
