f2e9925e9447d005e004bcb7b91b9c3f8f1a9665
Ben Davis <joemunene91@gmail.com>
2025-11-13 21:22:43 +0300

    h

--- public/js/auth/checkout.js
+++ public/js/auth/checkout.js
@@ -129,7 +129,7 @@ const showNotification = () => {
 
 		setTimeout(() => { pdfFunction(); }, 5000);
 
-		setTimeout(() => { document.getElementsByClassName('toast')[0].classList.add(`anonz`); }, 200);
+		setTimeout(() => { document.getElementsByClassName('toast')[0].classList.add(`anons`); }, 200);
 		var shortCutFunction = 'success';var msg = `${nextLine} <hr class="to-hr hr15-top">`;
 		toastr.options =  { closeButton: true, debug: false, newestOnTop: true, timeOut: 4000,progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null };
 		var $toast = toastr[shortCutFunction](msg); $toastlast = $toast;
