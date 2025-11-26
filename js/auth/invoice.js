8e0402588a92df9dd8fad7fad27aec0828d8efe9
Ben Davis <joemunene91@gmail.com>
2025-11-26 14:16:49 +0300

    Updates

--- js/auth/invoice.js
+++ js/auth/invoice.js
@@ -92,11 +92,11 @@ function emailVerified() {
 
 			setTimeout(() => { document.getElementsByClassName('toast')[0].classList.add(`anons`); }, 200);
 			var shortCutFunction = 'success'; var msg = `Email verified -- <br> ${email} <br> <hr class="to-hr hr15-top">`;
-			toastr.options =  { closeButton: true, debug: false, newestOnTop: true, timeOut: 4000,progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null }; var $toast = toastr[shortCutFunction](msg); $toastlast = $toast;
+			toastr.options =  { closeButton: true, debug: false, newestOnTop: true, timeOut: 5000,progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null }; var $toast = toastr[shortCutFunction](msg); $toastlast = $toast;
 		
 			setTimeout(() => {
 				window.location.assign('checkout');
-			}, 5000);
+			}, 6000);
 		}
 	});
 }
